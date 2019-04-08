379291b0a7e8e7129296413a4b3ca7ee2eaaa50e
Dave Laxman <david.laxman@ldschurch.org>
2019-04-08 15:55:15 -0600

    First committ

---
+++ src/main/java/com/SDETTest/SDETTest/SdetTestApplication.java
@@ -0,0 +1,13 @@
+package com.SDETTest.SDETTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdetTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdetTestApplication.class, args);
+	}
+
+}
